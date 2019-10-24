5bc09e74d618e5f75cea87bd6beaa72a69355208
Soro Didiouf <ssoro013@gmail.com>
2019-10-23 22:25:22 -0700

    Course Schedule

--- dp.js
+++ dp.js
@@ -25,3 +25,12 @@ var fib = function(n) {
         return fib(n - 1) + fib(n - 2);
     }
 };
+
+//Course Schedule
+//There are a total of n courses you have to take, labeled from 0 to n-1.
+//Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+//Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+var canFinish = function(numCourses, prerequisites) {
+    
+}
