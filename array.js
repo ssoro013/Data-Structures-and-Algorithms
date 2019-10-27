c4ca8144230a1832007a6e663122419219965501
Soro Didiouf <ssoro013@gmail.com>
2019-10-26 19:06:32 -0700

    Remove Element

---
+++ array.js
@@ -0,0 +1,29 @@
+//Array
+
+//Remove Duplicates
+//Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+//Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+var removeDuplicates1 = function(arr) {
+    for (var i = 0; i < arr.length - 1; i++) {
+        if(arr[i] === arr[i-1]) {
+            arr.splice(i,1);
+            i--
+        }
+    }
+    return arr.length;
+}
+
+//Remove Element
+//Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+//Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+//The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+var removeElement = function(arr, val) {
+    for (var i = 0; i < arr.length; i++) {
+        if(arr[i] === val) {
+            arr.splice(i, 1);
+            i--
+        }
+    }
+    return arr.length;
+}
