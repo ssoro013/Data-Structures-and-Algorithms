291c699d7817b424ae9291f5320677f61692c321
Soro Didiouf <ssoro013@gmail.com>
2019-10-28 21:44:01 -0700

    Champagne Tower

--- dp.js
+++ dp.js
@@ -1,10 +1,9 @@
 /* eslint-disable indent */
 //Dynamic Programming
 
-//Climbing Stairs
+//Climbing Stairs (https://leetcode.com/problems/climbing-stairs/)
 // You are climbing a stair case. It takes n steps to reach to the top.
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-
 var climbStairs = function(n) {
     if (n <= 2) { return n; }
     var arr = [1, 2];
@@ -14,10 +13,9 @@ var climbStairs = function(n) {
     return arr[n - 1];
 };
 
-//Fibonacci Number
+//Fibonacci Number (https://leetcode.com/problems/fibonacci-number/)
 //The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is 
 //the sum of the two preceding ones, starting from 0 and 1
-
 var fib = function(n) {
     if (n <= 1) {
         return n;
@@ -26,17 +24,30 @@ var fib = function(n) {
     }
 };
 
-//Course Schedule
+//Course Schedule (https://leetcode.com/problems/course-schedule/)
 //There are a total of n courses you have to take, labeled from 0 to n-1.
 //Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
 //Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
-
 var canFinish = function(numCourses, prerequisites) {
     
 }
 
-//Champagne Tower 
-
+//Champagne Tower (https://leetcode.com/problems/champagne-tower/)
 var champagneTower = function(poured, query_row, query_glass) {
-    
-}
+    var arr1 = [];
+    arr1[0] = [poured];
+    for (var row = 1; row <= query_row; row++) {
+        var arr2 = new Array(row + 1).fill(0);
+        for (var glass = 0; glass <= row; glass++) {
+            if (arr1[row - 1][glass - 1] && arr1[row - 1][glass - 1] > 1) {
+                arr2[glass] += (arr1[row - 1][glass - 1] - 1) / 2;
+            }
+
+            if (arr1[row - 1][glass] && arr1[row - 1][glass] > 1) {
+                arr2[glass] += (arr1[row - 1][glass] - 1) / 2;
+            }
+        }
+        arr1[row] = arr2;
+    }
+    return arr1[query_row][query_glass] > 1 ? 1 : arr1[query_row][query_glass];
+};
