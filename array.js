44447c89b9a24f7c5f054d1014499e06b7e74bcf
Soro Didiouf <ssoro013@gmail.com>
2019-11-06 11:49:46 -0800

    Trapping Rain Water

--- array.js
+++ array.js
@@ -38,20 +38,40 @@ var removeDuplicates2 = function(arr) {
 //Non-decreasing Array (https://leetcode.com/problems/non-decreasing-array/)
 var checkPossibility = function(nums) {
     var count = 0;
-    for (var i = 0; i < nums.length - 1; i++) {
-        if (nums[i] > nums[i + 1]) {
-            if (i > 0) {
-                if (nums[i - 1] <= nums[i + 1]) {
-                    nums[i] = nums[i - 1]
-                } else {
-                    nums[i + 1] = nums[i]
-                }
+    // for (var i = 0; i < nums.length - 1; i++) {
+    //     if (nums[i] > nums[i + 1]) {
+    //         if (i > 0) {
+    //             if (nums[i - 1] <= nums[i + 1]) {
+    //                 nums[i] = nums[i - 1];
+    //             } else {
+    //                 nums[i + 1] = nums[i];
+    //             }
+    //         }
+    //     }
+    // }
+    var count = 0;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] < nums[i - 1]) {
+            if (i - 2 >= 0 && nums[i] < nums[i - 2]) {
+                nums[i] = nums[i - 1];
             }
+        count++;
         }
     }
+  return count <= 1;
 };
 
 //Trapping Rain Water (https://leetcode.com/problems/trapping-rain-water/)
-var trap = function(arr) {
-    
+var trap = function(height) {
+    var l = height.length;
+    var collection = 0;
+    for (var i = 1; i < l - 1; i++) {
+        var maxLeft = Math.max(...height.slice(0, i));
+        var maxRight = Math.max(...height.slice(i + 1, l));
+        var water = Math.max(Math.min(maxLeft, maxRight) - height[i], 0);
+        collection += water;
+    }
+    return collection;
 }
+
+//Note: optimize for time and space complexity
