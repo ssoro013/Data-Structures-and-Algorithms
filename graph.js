0c3bf6753ff2aabac3fd8ad4f9b6f393d336e5c5
Soro Didiouf <ssoro013@gmail.com>
2019-11-12 12:05:01 -0800

    Implementation of graph

---
+++ graph.js
@@ -0,0 +1,52 @@
+class Graph {
+    constructor(vertices) {
+        this.vertices = vertices;//stores number of vertices in the graph
+        this.adjList = new Map();//stores the adjancy list of a particular vertex
+    }
+
+    addVertex(v) {
+        this.adjList.set(v, []);
+    }
+
+    addEdge(v, w) {
+        this.adjList.get(v).push(w);
+        this.adjList.get(w).push(v);
+    }
+
+    printGraph() {
+        var keys = this.adjList.keys();
+        for (var key of keys) {
+            var values = this.adjList.get(key);
+            var output = '';
+            for (var j = 0; j < values.length; j++) {
+                output += values[j] + ' ';
+            }
+            console.log(key +'->' + output);
+        }
+    }
+}
+var g = new Graph(6);
+var vertices = ['A', 'B', 'C', 'D', 'E', 'F'];
+for (var i = 0; i < vertices.length; i++) {
+    g.addVertex(vertices[i])
+}
+
+g.addEdge('A', 'B'); 
+g.addEdge('A', 'D'); 
+g.addEdge('A', 'E'); 
+g.addEdge('B', 'C'); 
+g.addEdge('D', 'E'); 
+g.addEdge('E', 'F'); 
+g.addEdge('E', 'C'); 
+g.addEdge('C', 'F'); 
+g.printGraph()
+
+//Number of Islands
+var numIslands = function(grid) {
+
+};
+
+//Cut Off Trees for Golf Event
+var cutOffTree = function(forest) {
+    
+};
