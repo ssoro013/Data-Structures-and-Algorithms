9939029f05f9642d8185dc56f5b3a9466e1b12d4
Soro Didiouf <ssoro013@gmail.com>
2019-12-10 11:59:32 -0800

    Merge sort

--- sort.js
+++ sort.js
@@ -7,3 +7,32 @@
 //Heap Sort
 
 //Merge Sort
+var mergSort = function (arr) {
+    var length = arr.length;
+    var mid = Math.floor(length / 2);
+    if (length < 2) {
+        return arr;
+    } else {
+        return merge(mergSort(arr.slice(0, mid)), mergSort(arr.slice(mid)));
+    }
+}
+
+var merge = function (left, right) {
+    var output = [];
+    var leftIndex = 0;
+    var rightIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            output.push(left[leftIndex]);
+            leftIndex ++;
+        } else {
+            output.push(right[rightIndex]);
+            rightIndex ++;
+        }
+    }
+    return output.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
+}
+
+var arr = [9, 1, 0, 5, 7, 7];
+var output = mergSort(arr);
+console.log(output)
