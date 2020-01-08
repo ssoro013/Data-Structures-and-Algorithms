c3d24a57443d1bb35935511e654eda485d34a042
Soro Didiouf <ssoro013@gmail.com>
2020-01-07 19:14:11 -0800

    Removed unnecessary comments

--- dp.js
+++ dp.js
@@ -2,8 +2,6 @@
 //Dynamic Programming
 
 //Climbing Stairs (https://leetcode.com/problems/climbing-stairs/)
-// You are climbing a stair case. It takes n steps to reach to the top.
-// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 var climbStairs = function(n) {
     if (n <= 2) { return n; }
     var arr = [1, 2];
@@ -14,8 +12,6 @@ var climbStairs = function(n) {
 };
 
 //Fibonacci Number (https://leetcode.com/problems/fibonacci-number/)
-//The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is 
-//the sum of the two preceding ones, starting from 0 and 1
 var fib = function(n) {
     if (n <= 1) {
         return n;
