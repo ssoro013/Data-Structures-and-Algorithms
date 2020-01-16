6881663a868976a1aa20c348fdd6b5f202607ac3
Soro Didiouf <ssoro013@gmail.com>
2020-01-15 18:34:27 -0800

    Fix indentation

--- linkedList.js
+++ linkedList.js
@@ -1,175 +1,167 @@
 var LinkedList = function() {
-  this.head = null;
+    this.head = null;
 };
 
 var Node = function(value) {
-  return {value: value, next: null};
+    return {value: value, next: null};
 };
 
 LinkedList.prototype.insert = function(value) {
-  const node = new Node(value);
-  if (!this.head) {
-    this.head = node;
+    const node = new Node(value);
+    if (!this.head) {
+        this.head = node;
   } else {
-    var current = this.head;
-    while (current.next) {
-      current = current.next;
+      var current = this.head;
+      while (current.next) {
+            current = current.next;
+        }
+        current.next = node;
     }
-    current.next = node;
-  }
 };
 
 LinkedList.prototype.getValues = function() {
-  var arr = [];
-  var current = this.head;
-  while (current) {
-    arr.push(current.value);
-    current = current.next;
+    var arr = [];
+    var current = this.head;
+    while (current) {
+        arr.push(current.value);
+        current = current.next;
   }
-  return arr;
+    return arr;
 };
 
 //Middle of Linked List (https://leetcode.com/problems/middle-of-the-linked-list/)
 LinkedList.prototype.middleNode = function() {
-  var arr = [];
-  var length = 0;
-  var current = this.head;
-  while (current) {
-    arr.push(current);
-    current = current.next;
-    length ++;
-  }
-  var middle = Math.ceil((length - 1) / 2);
-  return arr[middle];
+    var arr = [];
+    var length = 0;
+    var current = this.head;
+    while (current) {
+        arr.push(current);
+        current = current.next;
+        length ++;
+    }
+    var middle = Math.ceil((length - 1) / 2);
+    return arr[middle];
 };
 
-var List = new LinkedList();
-List.insert(1);
-List.insert(2);
-List.insert(3);
-List.insert(4);
-List.insert(5);
-console.log(List.middleNode());
-
 LinkedList.prototype.reverse = function() {
-  var current = this.head;
-  var previous = null;
-  while (current) {
-    var next = current.next;
-    current.next = previous;
-    previous = current;
-    current = next;
-  }
-  this.head = previous;
+    var current = this.head;
+    var previous = null;
+    while (current) {
+        var next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
+    }
+    this.head = previous;
 };
 
 LinkedList.prototype.reverseBetween = function(m, n) {
-  var count = 1;
-  var current = this.head;
-  var previous = null;
-  while (current) {
-    if (count < m) {
-      current = current.next;
-      previous = current;
-      count++;
-    } else if (count >= m && count <= n) {
-      var next = current.next;
-      current.next = previous;
-      previous = current;
-      current = next;
-      count++;
-    } else if (count > n) {
-      return;
+    var count = 1;
+    var current = this.head;
+    var previous = null;
+    while (current) {
+        if (count < m) {
+            current = current.next;
+            previous = current;
+            count++;
+        } else if (count >= m && count <= n) {
+            var next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+            count++;
+        } else if (count > n) {
+            return;
+        }
     }
-  }
   // return head;
 };
 
 LinkedList.prototype.deleteNode = function(value) {
-  var current = this.head;
-  while (current) {
-    if (current.next.value === value) {
-      current.next = current.next.next;
-      return;
-    } else {
-      current = current.next;
+    var current = this.head;
+    while (current) {
+        if (current.next.value === value) {
+            current.next = current.next.next;
+            return;
+        } else {
+            current = current.next;
+        }
     }
-  }
 };
 
 LinkedList.prototype.deleteDuplicates = function() {
-  var current = this.head;
-  while (current && current.next) {
-    if (current.value === current.next.value) {
-      current.next = current.next.next;
-    } else {
-      current = current.next;
+    var current = this.head;
+    while (current && current.next) {
+        if (current.value === current.next.value) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
     }
-  }
 };
 
 LinkedList.prototype.isPalindrome = function() {
-  var current = this.head;
-  var arr = [];
-  while (current) {
-    arr.push(current.value);
-    current = current.next;
+    var current = this.head;
+    var arr = [];
+    while (current) {
+        arr.push(current.value);
+        current = current.next;
   }
-  if (arr.length === 0) {
-    return true;
-  }
-
-  var reversed = arr.reverse();
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] !== reversed[arr.length - 1 - i ]) {
-      return false;
+    if (arr.length === 0) {
+        return true;
     }
-  }
-  return true;
+
+    var reversed = arr.reverse();
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] !== reversed[arr.length - 1 - i ]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 LinkedList.prototype.hasCycle = function() {
-  var slow = this.head;
-  var fast = this.head;
-  while (slow && fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) {
-      return true;
+    var slow = this.head;
+    var fast = this.head;
+    while (slow && fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 };
 
 //Add Two Numbers (https://leetcode.com/problems/add-two-numbers/)
 var addTwoNumbers = function () {
-  
+
 }
 
 //Rotate List (https://leetcode.com/problems/rotate-list/)
 
 //Remove Linked List Elements (https://leetcode.com/problems/remove-linked-list-elements/)
 var removeElements = function (head, val) {
-  var head = head;
-  while (head && head.val === val) {
-    head = head.next;
-  }
-
-  if (!head) {
-    return null;
-  }
-
-  var current = head;
-  var previous = head;
-  while (current) {
-    if (current.val !== val) {
-      previous = current;
-      current = current.next
-    } else {
-      previous.next = current.next;
-      current = current.next;
+    var head = head;
+    while (head && head.val === val) {
+        head = head.next;
     }
-  }
-  return head
+
+    if (!head) {
+        return null;
+    }
+
+    var current = head;
+    var previous = head;
+    while (current) {
+        if (current.val !== val) {
+            previous = current;
+            current = current.next
+        } else {
+            previous.next = current.next;
+            current = current.next;
+        }
+    }
+    return head
 }
 //Metrics: runtime of 68ms faster than 90% and memory usage of 37mb less than 50% of online submissions
