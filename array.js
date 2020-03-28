9b2f407e1b94b0efd2257bdd9b71408cd612a43e
Soro Didiouf <ssoro013@gmail.com>
2020-03-27 23:51:52 -0700

    Single Number

--- array.js
+++ array.js
@@ -377,3 +377,13 @@ var sortKMessedArray = function(arr, k) {
     }
     return arr;
 }
+
+//Single Number (https://leetcode.com/problems/single-number/)
+var singleNumber = function(nums) {
+    nums.sort((a,b) => a - b);
+    for(let i = 0; i < nums.length; i += 2) {
+        if(nums[i] !== nums[i + 1]) {
+            return nums[i]
+        }
+    }
+}
