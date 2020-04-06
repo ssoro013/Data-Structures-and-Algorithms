a5ea497ddb0784351038f6876b96db956d559b86
Soro Didiouf <ssoro013@gmail.com>
2020-04-05 17:48:02 -0700

    Best Time to Buy and Sell Stock II

--- array.js
+++ array.js
@@ -510,3 +510,15 @@ var moveZeroes = function(nums) {
     }
     return nums;
 };
+
+//Best Time to Buy and Sell Stock
+//Best Time to Buy and Sell Stock II(https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/)
+var maxProfit = function(prices) {
+    let max = 0;
+    for(let i = 1; i < prices.length; i++) {
+        if(prices[i] > prices[i - 1]) {
+            max += prices[i] - prices[i - 1];
+        }
+    }
+    return max;
+}
