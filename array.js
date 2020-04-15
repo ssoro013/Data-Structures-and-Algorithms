8aee5cacba45bca8c1f2a8ce239f48dec51ec3b8
Soro Didiouf <ssoro013@gmail.com>
2020-04-14 17:41:57 -0700

    String Shift

--- array.js
+++ array.js
@@ -593,3 +593,22 @@ var findMaxLength = function(nums) {
     }
     return max;
 };
+
+//Perform String Shifts
+var stringShift = function(s, shift) {
+    let chars = s.split('');
+    for(let i = 0; i < shift.length; i++) {
+        if(shift[i][0] === 0) {
+            while(shift[i][1] > 0) {
+                chars.push(chars.shift());
+                shift[i][1]--;
+            }
+        } else {
+            while(shift[i][1] > 0) {
+                chars.unshift(chars.pop());
+                shift[i][1]--;
+            }
+        }
+    }
+    return chars.join('')
+};
