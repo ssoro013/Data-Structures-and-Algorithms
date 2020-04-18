12cd9502bda4fb44caa7d5e38808d825116e8640
Soro Didiouf <ssoro013@gmail.com>
2020-04-17 22:11:32 -0700

    Stack

--- stack.js
+++ stack.js
@@ -22,3 +22,29 @@ class Stack {
     }
 }
 
+//Min Stack (https://leetcode.com/problems/min-stack/)
+var MinStack = function() {
+    this.stack = [];
+    this.min = Infinity;
+}
+
+MinStack.prototype.push = function(x) {
+    this.min = Math.min(this.min, x);
+    this.stack.push({
+        val: x,
+        min: this.min
+    })
+}
+
+MinStack.prototype.pop = function() {
+    this.stack.pop();
+    this.min = this.stack.length > 0 ? this.stack[this.stack.length - 1].min : Infinity;
+}
+
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.lenth - 1];
+}
+
+MinStack.prototype.getMin = function() {
+    return this.min;
+}
